2f2aedba7831861b7aabfa3f260916ef019ac3d5
lschimm <lschimmelfennig@gmail.com>
2020-03-11 13:57:22 -0500

    adds needed components

--- pokedex-react/src/components/layout/NavBar.js
+++ pokedex-react/src/components/layout/NavBar.js
@@ -5,7 +5,7 @@ export default class NavBar extends Component {
     render() {
         return (
             <div className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-            <a>Pokedex</a>
+            <a href="#" className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center">Pokedex</a>
             </div>
         )
     }
---
+++ pokedex-react/src/components/pokemon/PokemonCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class PokemonCard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PokemonCard;
