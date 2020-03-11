44b67ce6d1683786eefe7a9bc1c14e9e79157921
lschimm <lschimmelfennig@gmail.com>
2020-03-11 14:15:04 -0500

    hooks up to the pokeapi

--- pokedex-react/src/components/pokemon/PokemonList.js
+++ pokedex-react/src/components/pokemon/PokemonList.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import PokemonCard from './PokemonCard'
 
 export default class PokemonList extends Component {
+    // get pokemon list!
+    state= {
+        url: "https://pokeapi.co/api/v2/pokemon",
+        pokemon: null
+    }
+    //visible to the user
+    async componentDidMount() {
+        const res = await axios.get(this.state.url)
+        this.setState({pokemon: res.data['results'] })
+        // this.setState to rerun 
+    }
     render() {
         return (
             <div>
+            {this.state.pokemon ? (<div className="row">
+            {this.state.pokemon.map((pokemon) => (
                 <PokemonCard />
-            </div>
+                ))}
+        </div>) : (<h1>Loading Pokemon...</h1>)}
+        </div>
         )
     }
 }
