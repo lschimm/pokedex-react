50932eb32614315f38168be13bb116f7d3ebcab9
lschimm <lschimmelfennig@gmail.com>
2020-03-12 12:16:40 -0500

    adds simple styling for pokemon cards

--- pokedex-react/src/components/pokemon/PokemonCard.js
+++ pokedex-react/src/components/pokemon/PokemonCard.js
@@ -9,6 +9,14 @@ const Sprite = styled.img`
     display: none;
 `
 
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover {
+        box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    }
+`
+
 export class PokemonCard extends Component {
     state = {
         name: '',
@@ -35,37 +43,37 @@ export class PokemonCard extends Component {
     render() {
         return (
             <div className="col-md-3 col-sm-6 mb-5">
-            <div className="card">
-                <h5 className="card-header">{this.state.pokemonIndex}</h5>
-                {this.state.imageLoading ? (
-                    <img src={spinner} style={{width: '5em', height: '5em' }} className="card-img-top rounded mx-auto d-block mt-2"></img>
-                ) : null }
-                <Sprite 
-                className="card-img-top rounded mx-auto mt-2" 
-                onLoad={() => this.setState({imageLoading: false})}
-                onError={() => this.setState({tooManyRequests: true})}
-                src={this.state.imageUrl} 
-                style={
-                    this.state.tooManyRequests ? { display: "none"} :
-                    this.state.imageLoading ? null : {display: "block"}
-                }
-                />
-                {this.state.tooManyRequests ? (
-                    <h6 className='mx-auto'>
-                        <span className="badge badge-danger mt-2">Too Many Requests :(</span>
-                    </h6>
-                ) : null }
-                <div className="card-body mx-auto">
-                    <h6 className="card-title">
-                        {this.state.name
-                        .toLowerCase() // set it to lower case
-                        .split(' ') // splitting it at the space
-                        .map((letter) => letter.charAt(0).toUpperCase() + letter.substring(1)) // mapping over each, sets it to upper case 
-                        // then BOOM. joins it together
-                        .join(' ')}
-                    </h6>
-                </div>
-            </div>
+                <Card className="card">
+                    <h5 className="card-header">{this.state.pokemonIndex}</h5>
+                    {this.state.imageLoading ? (
+                        <img src={spinner} style={{width: '5em', height: '5em' }} className="card-img-top rounded mx-auto d-block mt-2"></img>
+                    ) : null }
+                    <Sprite 
+                    className="card-img-top rounded mx-auto mt-2" 
+                    onLoad={() => this.setState({imageLoading: false})}
+                    onError={() => this.setState({tooManyRequests: true})}
+                    src={this.state.imageUrl} 
+                    style={
+                        this.state.tooManyRequests ? { display: "none"} :
+                        this.state.imageLoading ? null : {display: "block"}
+                    }
+                    />
+                    {this.state.tooManyRequests ? (
+                        <h6 className='mx-auto'>
+                            <span className="badge badge-danger mt-2">Too Many Requests :(</span>
+                        </h6>
+                    ) : null }
+                    <div className="card-body mx-auto">
+                        <h6 className="card-title">
+                            {this.state.name
+                            .toLowerCase() // set it to lower case
+                            .split(' ') // splitting it at the space
+                            .map((letter) => letter.charAt(0).toUpperCase() + letter.substring(1)) // mapping over each, sets it to upper case 
+                            // then BOOM. joins it together
+                            .join(' ')}
+                        </h6>
+                    </div>
+                </Card>
             </div>
         );
     }
