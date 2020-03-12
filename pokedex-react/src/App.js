0766328c3e643bec8430176c5c668793bb589635
lschimm <lschimmelfennig@gmail.com>
2020-03-12 12:35:10 -0500

    Adds background image and fixes navbar color

--- pokedex-react/src/App.js
+++ pokedex-react/src/App.js
@@ -5,11 +5,12 @@ import './App.css'
 
 import NavBar from './components/layout/NavBar'
 import Dashboard from './components/layout/Dashboard'
+import backgroundImage from './components/img/pattern.png'
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{background: `url(${backgroundImage})`}}>
         <NavBar />
         <div className="container">
           <Dashboard />
